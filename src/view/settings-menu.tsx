074c8eec39133b738a52c3e7c5c7c5cc2d6c2deb
Crawron <azertyaaron@gmail.com>
2021-12-15 20:01:54 -0500

    Finish* settings screen

--- src/view/settings-menu.tsx
+++ src/view/settings-menu.tsx
@@ -1,13 +1,19 @@
-import { Newline, Text, useFocus } from "ink"
+import { Newline, Text } from "ink"
 import React, { useState } from "react"
-import { TextInput } from "../ink-text-input"
 import { Setting } from "../setting"
 import type { RouterStore } from "../stores/router-store"
+import {
+  ButtonInput,
+  CheckboxInput,
+  FocusableList,
+  LabeledTextInput,
+} from "./focusable-list"
 import { MenuScreenLayout } from "./menu-screen-layout"
 
 export function SettingsMenu({ routerStore }: { routerStore: RouterStore }) {
   const botToken = new Setting<string>("botToken", "")
   const userId = new Setting<string>("userId", "")
+  const notify = new Setting<boolean>("notify", false)
   const guildId = new Setting<string>("guildId", "")
 
   const [changed, setChanged] = useState(0)
@@ -15,74 +21,52 @@ export function SettingsMenu({ routerStore }: { routerStore: RouterStore }) {
   return (
     <>
       <MenuScreenLayout.Section>
-        <SettingItem
-          sensitive
-          label="Bot Token"
-          value={botToken.get()}
-          onChange={(value) => {
-            botToken.set(value)
-            setChanged(changed + 1)
-          }}
-        />
-        <SettingItem
-          label="User ID"
-          value={userId.get()}
-          onChange={(value) => {
-            userId.set(value)
-            setChanged(changed + 1)
-          }}
-        />
-        <SettingItem
-          label="Guild ID"
-          value={guildId.get()}
-          onChange={(value) => {
-            guildId.set(value)
-            setChanged(changed + 1)
-          }}
-        />
-
+        <FocusableList>
+          <LabeledTextInput
+            label="Bot Token"
+            sensitive
+            onChange={(value) => {
+              botToken.set(value)
+              setChanged(changed + 1)
+            }}
+            value={botToken.get()}
+          />
+          <LabeledTextInput
+            label="User ID"
+            onChange={(value) => {
+              userId.set(value)
+              setChanged(changed + 1)
+            }}
+            value={userId.get()}
+          />
+          <LabeledTextInput
+            label="Guild ID"
+            onChange={(value) => {
+              guildId.set(value)
+              setChanged(changed + 1)
+            }}
+            value={guildId.get()}
+          />
+          <CheckboxInput
+            label="Notify on source change"
+            value={notify.get()}
+            onChange={(value) => {
+              notify.set(value)
+              setChanged(changed + 1)
+            }}
+          />
+          <Newline count={1} />
+          <ButtonInput
+            onPress={() => routerStore.setScreen("main")}
+            label="Back"
+          />
+        </FocusableList>
         {!!changed && (
           <Text color="green">
-            <Newline />
             Settings saved! {changed > 1 && `(${changed})`}
           </Text>
         )}
       </MenuScreenLayout.Section>
-    </>
-  )
-}
-
-function SettingItem({
-  label,
-  value,
-  onChange,
-  sensitive,
-}: {
-  label: string
-  value: string
-  onChange: (value: string) => void
-  sensitive?: boolean
-}) {
-  const { isFocused } = useFocus()
-
-  const [internalValue, setInternalValue] = useState<string>(value)
-
-  const color = isFocused ? "blue" : "white"
-  const bold = isFocused ? true : false
-
-  return (
-    <>
-      <Text bold={bold} color={color}>
-        {label}:{" "}
-        <TextInput
-          focus={isFocused}
-          value={internalValue}
-          placeholder="None"
-          onChange={setInternalValue}
-          mask={!((!isFocused && !sensitive) || isFocused) ? "*" : undefined}
-          onSubmit={onChange}
-        />
-      </Text>
     </>
   )
 }
